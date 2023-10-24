00fd4dfc68dda4c04d3809c3801236dafdddb7cb
jayakrishna <kommurujayakrishna@gmail.com>
2023-10-24 18:36:22 +0530

    update commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app=express();
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/crud")
+mongoose.connect(process.env.DATABASE)
 
 app.get('/users',(req,res)=>{
     UserModel.find({})
